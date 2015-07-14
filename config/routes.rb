f5deb44682065dd6b9c8177809c70f41b485d679
Josh Prince <josh.prince@arcadiasolutions.com>
2015-07-13 22:11:30 -0400

    Add route for DELETE method and keeper edit path
    This is a workaround needed to make the test for removing keepers (in league_manager_spec)
    pass. There's currently a bug in PhantomJS (https://github.com/ariya/phantomjs/issues/11214)
    that causes PhantomJS to make a request for 303 redirect with the original request method,
    in this case DELETE instead of a GET.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     resource :draft, only: :show
     resource :draft_results, only: [:create, :show]
     resource :keepers, only: [:create, :destroy, :edit]
+    # workaround for PhantomJS bug https://github.com/ariya/phantomjs/issues/11214
+    # league_manager_spec/can manage keepers after picks are generated will fail without this route
+    delete '/keepers/edit', to: 'keepers#edit'
     resources :teams
     get 'draft_order',
       to: 'draft_order#edit'
